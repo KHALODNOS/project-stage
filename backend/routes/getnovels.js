2628cf150456458b70fed9d5952e60e06db3e40b
Khalid Moumine <khalidmoumine02@gmail.com>
2026-03-14 22:56:58 +0000

    add novelCategory controller

--- backend/routes/getnovels.js
+++ backend/routes/getnovels.js
@@ -1,57 +1,26 @@
-const express = require('express');
-const Novel = require('../models/novel'); // Adjust the path to your model
+const express = require("express");
+const Novel = require("../models/novel"); // Adjust the path to your model
 const router = express.Router();
+const {
+  Popular,
+  Updated,
+  Completed,
+  New,
+  Views,
+} = require("../controller/NovelCategoryController");
+
 // Get popular novels
-router.get(['/popular', '/popular/:viewType'], async (req, res) => {
-  const viewType = req.params.viewType || 'daily';
-  try {
-    const novels = await Novel.find().sort({ [`views.${viewType}`]: -1 }).limit(10);
-    res.json(novels);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+router.get(["/popular", "/popular/:viewType"], Popular);
 
 // Get recently updated novels
-router.get('/updated', async (req, res) => {
-  try {
-    const novels = await Novel.find().sort({ updatedAt: -1 }).limit(9);
-    res.json(novels);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+router.get("/updated", Updated);
 
 // Get completed novels
-router.get('/completed', async (req, res) => {
-  try {
-    const novels = await Novel.find({ status: 'مكتملة' }).sort({ updatedAt: -1 }).limit(9);
-    res.json(novels);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+router.get("/completed", Completed);
 
 // Get new novels
-router.get('/new', async (req, res) => {
-  try {
-    const novels = await Novel.find().sort({ createdAt: -1 }).limit(15);
-    res.json(novels);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
+router.get("/new", New);
 
 // Route to fetch novels by views
-router.get('/views/:viewType', async (req, res) => {
-  const { viewType } = req.params;
-  try {
-    const novels = await Novel.find().sort({ [`views.${viewType}`]: -1 }).limit(5);
-    res.json(novels);
-  } catch (error) {
-    res.status(500).json({ error: 'Error fetching novels' });
-  }
-});
+router.get("/views/:viewType", Views);
 module.exports = router;
-
